918675063f2ae183af354c439be864b6daf5f28b
arownz <pasionharold01@gmail.com>
2025-08-15 16:15:28 +0800

    web export

--- WebTest/index.service.worker.js
+++ WebTest/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1755156488|23137560668';
+const CACHE_VERSION = '1755243617|335071065';
 /** @type {string} */
 const CACHE_PREFIX = 'Lexia-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
